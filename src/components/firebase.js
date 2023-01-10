585239968f60966046fc75122a942a67a91418eb
mykfrost <okellofrost@gmail.com>
2023-01-10 12:26:21 +0300

    second commit

---
+++ src/components/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCcmWSqLkAUfwQTUQuN2iUd8PBANj1J-n4",
+  authDomain: "todo-97d83.firebaseapp.com",
+  projectId: "todo-97d83",
+  storageBucket: "todo-97d83.appspot.com",
+  messagingSenderId: "214954938358",
+  appId: "1:214954938358:web:9eb295e16c10ced8e681d7",
+  measurementId: "G-17V6ZM8HBQ"
+};
