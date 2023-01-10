17ce53439c3ddca757a35314d09d5f55daa04b7f
mykfrost <okellofrost@gmail.com>
2023-01-10 13:01:29 +0300

    first list of todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos , setTodos] = useState(['Make a react firebase project' , 'Record a coding video']);
+  const [input , setInput] = useState(['']);
+  const addTodo = e => {
+    e.preventDefault();
+    setTodos([...todos ,input]);
+    setInput('');
+  }
   return (
     <div className="App">
-      
-        
+      <h1>ToDo App by Mike Frost</h1>
+        <form>
+          <input value={input} onChange={e => setInput(e.target.value)}></input>
+          <button type='submit' onClick={addTodo}>Add Todo</button>
+        </form>
+        <ul>
+          {todos.map(todo => <li>{todo}</li>)}
+        </ul>
     </div>
   );
 }
